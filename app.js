d8e59e6c73e238786105543e32b8cf709503475d
Juan Pablo Gaviria Salazar <jgavirias13@gmail.com>
2020-02-09 18:53:43 -0500

    Configuracion de middlewares: Parser de json Recursos estaticos

--- app.js
+++ app.js
@@ -2,7 +2,9 @@ const express = require('express');
 const server = express();
 const configs = require('./config/index');
 
-
+//Configuracion de recursos estaticos
+server.use(express.static('./public'));
+server.use(express.json());
 
 server.listen(configs.PORT, () => {
     console.log(`El servidor se encuentra escuchando en el ${configs.PORT}`);
