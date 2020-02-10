3bab8d67606e5bbc575b6c89bcad798dbd3b7ea3
Juan Pablo Gaviria Salazar <jgavirias13@gmail.com>
2020-02-09 20:29:53 -0500

    Configuración de quotes: Se realiza configuración de metodos: - Agregar - Consultar Se realizan pruebas exitosas

--- controllers/quoteController.js
+++ controllers/quoteController.js
@@ -1,10 +1,30 @@
 const util = require('./util');
 const path = require('path');
 const db = require('../data/db.json');
+const DBPATH = path.join(__dirname + '/../data/db.json');
+const fs = require('fs');
 
 class QuoteController{
     async index(req, res){
         return util.render('quotes', res);
+    }
+
+    async get(req, res){
+        return res.send(db);
+    }
+
+    async add(req, res){
+        const quote = req.body;
+        const lastQuote = db[db.length - 1];
+        if(lastQuote){
+            const id = lastQuote.id;
+            quote.id = id + 1;
+        }else{
+            quote.id = 1;
+        }
+        db.push(quote);
+        fs.writeFileSync(DBPATH, JSON.stringify(db));
+        return res.status(201).send("Agregado exitosamente");
     }
 }
 
--- routes/quoteRoutes.js
+++ routes/quoteRoutes.js
@@ -1,5 +1,7 @@
-const controller = require('../controllers/quoteController');
+const controller = require("../controllers/quoteController");
 
-module.exports = (router) => {
-    router.get('/quotes', controller.index);
-}
+module.exports = router => {
+  router.get("/quotes", controller.index)
+        .post("/quotes", controller.add);
+  router.get("/quotes/all", controller.get);
+};
