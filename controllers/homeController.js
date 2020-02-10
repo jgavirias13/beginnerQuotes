c7811f3c4178d8ed832096f74a3d3f1954798c3f
Juan Pablo Gaviria Salazar <jgavirias13@gmail.com>
2020-02-09 19:24:17 -0500

    Configuración de route about. Renderizado de pagina por funcion de util

--- controllers/homeController.js
+++ controllers/homeController.js
@@ -1,8 +1,12 @@
 const util = require('./util');
 
 class HomeController {
-    async index(req, res) {
+    async index(req, res){
         return util.render('home', res);
+    }
+
+    async about(req, res){
+        return util.render('about', res);
     }
 }
 
--- routes/homeRoutes.js
+++ routes/homeRoutes.js
@@ -2,4 +2,5 @@ const controller = require('../controllers/homeController');
 
 module.exports = (router) => {
     router.get('/', controller.index);
+    router.get('/about', controller.about);
 };
