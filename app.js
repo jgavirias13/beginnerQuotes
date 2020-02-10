f0358c09bbe75c4f12c3f44734c1941593d76f53
Juan Pablo Gaviria Salazar <jgavirias13@gmail.com>
2020-02-09 19:13:26 -0500

    Configuracion de middlewares: NOT FOUND 404

--- app.js
+++ app.js
@@ -2,11 +2,13 @@ const express = require('express');
 const server = express();
 const configs = require('./config');
 const router = require('./routes');
+const notFoundMid = require('./middlewares/notFound');
 
 //Configuracion de recursos estaticos
 server.use(express.static('./public'));
 server.use(express.json());
 server.use('/', router);
+server.use(notFoundMid);
 
 server.listen(configs.PORT, () => {
     console.log(`El servidor se encuentra escuchando en el ${configs.PORT}`);
